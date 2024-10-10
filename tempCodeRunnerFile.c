190b23f056239452b29cad7cef628112b4bc29db
은수 Eunsu <sublimage1109@gmail.com>
2024-10-10 09:34:57 +0900

    1010

--- tempCodeRunnerFile.c
+++ tempCodeRunnerFile.c
@@ -1,15 +1,6 @@
-#include <stdio.h>
+#물건 리스트
+items = ["물병", "텐트", "지도", "책"]
 
-int main() {
-    printf("Size of char: %lu byte\n", sizeof(char));
-    printf("Size of short: %lu bytes\n", sizeof(short));
-    printf("Size of int: %lu bytes\n", sizeof(int));
-    printf("Size of long: %lu bytes\n", sizeof(long));
-    printf("Size of long long: %lu bytes\n", sizeof(long long));
-    printf("Size of float: %lu bytes\n", sizeof(float));
-    printf("Size of double: %lu bytes\n", sizeof(double));
-    printf("Size of long double: %lu bytes\n", sizeof(long double));
-    printf("Size of pointer: %lu bytes\n", sizeof(void*));
-
-    return 0;
-}
+#물병이 몇 번째 인덱스에 있는지 찾기
+index_of_bottle = items.index("물병")
+print(f"물병은 {index_of_bottle}번째 인덱스에 있습니다.")
