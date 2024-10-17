14c17aae9c974913ae12e9b60bf491afcdf7fd0a
은수 Eunsu <sublimage1109@gmail.com>
2024-10-17 13:40:07 +0900

    Create FibonacciSequence1.c

---
+++ FibonacciSequence1.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int fib1(int n) {
+    if (n <= 1)
+        return n;
+    else
+        return fib1(n - 1) + fib1(n - 2); // fib1 함수가 재귀적으로 호출됨
+}
+
+int main() {
+    int n;
+    printf("Enter the value of N: ");
+    scanf("%d", &n);
+
+    printf("Fibonacci number at position %d is %d\n", n, fib1(n));
+    
+    return 0;
+}
+10
